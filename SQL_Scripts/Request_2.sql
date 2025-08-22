4f7a5769df88ec80f18378ca15996212b6c0b1d7
Saksham Singh <160140188+saksham2508@users.noreply.github.com>
2025-08-22 17:36:14 +0530

    Add files via upload

---
+++ SQL_Scripts/Request_2.sql
@@ -0,0 +1,21 @@
+/*
+2. What is the percentage of unique product increase in 2021 vs. 2020? The
+final output contains these fields,
+unique_products_2020
+unique_products_2021
+percentage_chg
+*/
+
+WITH x AS
+	(SELECT COUNT(DISTINCT product_code) AS unique_products_2020
+    FROM fact_sales_monthly
+    WHERE fiscal_year=2020),
+y AS
+	(SELECT COUNT(DISTINCT product_code) AS unique_products_2021
+    FROM fact_sales_monthly
+    WHERE fiscal_year=2021)
+SELECT 
+	x.unique_products_2020,
+    y.unique_products_2021,
+    ROUND(((y.unique_products_2021-x.unique_products_2020)/x.unique_products_2020)*100,2)
+AS percentage_chg FROM x,y;
---
+++ SQL_Scripts/Request_5.sql
@@ -0,0 +1,26 @@
+/*
+5. Get the products that have the highest and lowest manufacturing costs.
+The final output should contain these fields,
+product_code
+product
+manufacturing_cost
+*/
+
+SELECT 
+	p.product_code,
+	p.product,
+	m.manufacturing_cost
+FROM 
+	dim_product AS p
+JOIN 
+	fact_manufacturing_cost AS m
+ON 
+	p.product_code = m.product_code
+WHERE 
+	m.manufacturing_cost = (SELECT 
+							MAX(manufacturing_cost) 
+							FROM fact_manufacturing_cost)
+OR
+	m.manufacturing_cost = (SELECT 
+							MIN(manufacturing_cost) 
+                            FROM fact_manufacturing_cost);
